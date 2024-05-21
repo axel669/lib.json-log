4e5a7c358672e38dd0cc9f9e53c416eed6161556
axel669 <haryuu.hanekata@gmail.com>
2024-05-21 11:14:07 -0700

    [change] use proper console functions internally

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const logger = (basectx = {}) => {
-    const write = (message, context, logLevel) => console.log(
-        JSON.stringify({ context, message, logLevel })
+    const write = (message, context, logLevel) => console[logLevel](
+        JSON.stringify({ context, message })
     )
 
     return {
         debug: (msg, ctx) => write(msg, { ...basectx, ...ctx }, "debug"),
-        info: (msg, ctx) => write(msg, { ...basectx, ...ctx }, "info"),
+        info: (msg, ctx) => write(msg, { ...basectx, ...ctx }, "log"),
         warn: (msg, ctx) => write(msg, { ...basectx, ...ctx }, "warn"),
         error: (msg, ctx) => write(msg, { ...basectx, ...ctx }, "error"),
         child: (extraCtx) => logger({ ...basectx, ...extraCtx }),
